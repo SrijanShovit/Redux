e78a6884649302706dd83a89da37b4c6c9f800e7
Srijan Shovit <srijanshovit@gmail.com>
2021-11-24 05:41:47 +0530

    combining mutiple reducres

--- index.js
+++ index.js
@@ -1,48 +1,95 @@
 const redux = require('redux');
-const {createStore } = redux;
+const {createStore,combineReducers} = redux;
 
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 
 
-//action creator is a function that returns an action
 function buyCake(){
-     //action is an object having type property
+  
     return {
         type:BUY_CAKE,
         info:'First redux action'
     }
 }
 
-//reducer is a function that accepts state and action as arguments, and returns the next state of  the application  ; initial state is also object
-const initialState = {
-    numOfCakes : 10
+function buyIcecream(){
+    return {
+        type:BUY_ICECREAM
+    }
 }
 
-const reducer = (state=initialState, action) => {
+// const initialState = {
+//     numOfCakes : 10,
+//     numOfIcecreams : 10,
+// }
+
+const initialCakeState = {
+    numOfCakes : 10,
+}
+
+const initialIcecreamState = {
+    numOfIcecreams : 15,
+}
+
+// const reducer = (state=initialState, action) => {
+//     switch (action.type) {
+//         case BUY_CAKE:return {
+           
+//             ...state,
+//             numOfCakes : state.numOfCakes - 1
+//         }
+//         case BUY_ICECREAM:return {
+//             ...state,
+//             numOfIcecreams : state.numOfIcecreams - 1
+//         }
+//         default:
+//             return state
+//     }
+// }
+const cakeReducer = (state=initialCakeState, action) => {
     switch (action.type) {
         case BUY_CAKE:return {
-            //we r telling reducer to make a copy of state and then update numOfCakes
+           
             ...state,
             numOfCakes : state.numOfCakes - 1
         }
+        
         default:
             return state
     }
 }
-//we r not mutating state object rather returning new object
 
-//creating store
-const store = createStore(reducer)
+const icecreamReducer = (state=initialIcecreamState, action) => {
+    switch (action.type) {
+        case BUY_ICECREAM:return {
+           
+            ...state,
+            numOfIcecreams : state.numOfIcecreams - 1
+        }
+        
+        default:
+            return state
+    }
+}
+
+const rootReducer = combineReducers({
+    cake:cakeReducer,
+    icecream:icecreamReducer
+})
+const store = createStore(rootReducer)
 console.log('Initial state',store.getState())
-//adding listener to store
+
 const unsubscribe = store.subscribe(()=> console.log('Updated state',store.getState()))
 
-//dispatching actions
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-store.dispatch(buyCake())
 
-//unsubscribe to any changes in store
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyIcecream())
+store.dispatch(buyIcecream())
+
+
 unsubscribe()
